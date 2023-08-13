7c637f368f82e615b566c059df3f91ce23b11200
lawlessowl <erokhin-maksim@bk.ru>
2023-08-13 22:07:42 +0300

    feat: add props for post

--- src/components/Content/Post/Post.jsx
+++ src/components/Content/Post/Post.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from './Post.module.css'
 
-function Post(props) {
-    return <div className={style.post}>  
+const Post = (props) => {
+  return  <div className={style.post_item}>
+      <img src={props.profile_icon} alt="user-img" className={style.user_img}/>
     {props.message}
+    <div className={style.like_box}>
+        <span className={style.like_count}>like</span>
+    </div>
     </div>
 }
 
--- src/components/Content/Posts/Posts.jsx
+++ src/components/Content/Posts/Posts.jsx
@@ -3,32 +3,18 @@ import style from './Posts.module.css'
 import Post from "../Post/Post";
 
 const Posts = () => {
-
-  let postsData = [
-    {id: 1, message: "Hello world!"},
-    {id: 2, message: "Goodby world!"}
-  ]
-
-  let postsElements = postsData
-  .map(p => <Post message={p.message}/>)
-  
-    return <div className={style.Posts}>
-        <div className={style.postsButton}>
+  return <div className={style.Posts}>
+    <div className={style.postsButton}>
       my posts
     </div>
     <div className={style.postsButton}>
       <textarea></textarea>
       <button>Add post</button>
     </div>
-    <div className={style.postsItem}>
-      <img src="https://i.pinimg.com/originals/e9/10/1a/e9101a6e66486645265e4241abf509ae.png" alt="user-img" className={style.user_img}/>
-    { postsElements[0] }
-    </div>
-    <div className={style.postsItem}>
-      <img src="https://i.pinimg.com/originals/28/e7/aa/28e7aa6558c41e93117273e0beee5609.jpg" alt="user-img" className={style.user_img}/>
-    { postsElements[1] }
-    </div>
-    </div>
+    <Post message = 'Hello world!' profile_icon = "https://i.pinimg.com/originals/e9/10/1a/e9101a6e66486645265e4241abf509ae.png"/>
+    <Post message = 'Have a nice day!' profile_icon = 'https://i.pinimg.com/originals/cd/cb/c3/cdcbc3a30cca0fc8a2ea8500d83e1076.jpg'/>
+    <Post message = 'I learn React!' profile_icon = 'https://i.pinimg.com/originals/bb/4c/89/bb4c894d6eff9e51254c5f90acf52fff.jpg'/>
+  </div>
 }
 
 export default Posts;
